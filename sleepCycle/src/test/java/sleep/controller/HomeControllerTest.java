e648bbcc41db122cbea430f36507e5eb49ed032f
stu235471 <stu235471@mail.uni-kiel.de>
2025-01-05 17:51:41 +0100

    fix docker issue

--- sleepCycle/src/test/java/sleep/controller/HomeControllerTest.java
+++ sleepCycle/src/test/java/sleep/controller/HomeControllerTest.java
@@ -3,6 +3,7 @@ package sleep.controller;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
